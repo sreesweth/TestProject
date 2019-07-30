8f0b27d34ca048de30060de2aff9f24da579db90
Gundappa <SGundappa@ap.org>
2019-07-29 20:58:03 -0400

    Added a registration form component

--- northwind/src/app/modules/login/login.module.ts
+++ northwind/src/app/modules/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 //--------------Modules,.
 
@@ -12,7 +13,7 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 
 @NgModule({
   declarations: [ // Declare any components that you create as part of this module
-    LoginFormComponent
+    LoginFormComponent, RegistrationFormComponent
   ],
   imports: [ //
     CommonModule,
